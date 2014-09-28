12ac21209ac283fa729f01ff63f00e841d5955bd
rstr74 <robbertstreng2012@gmail.com>
2014-09-29 00:12:06 +0200

    New version
    Some minor changes.

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ var fs = require("fs");
 (function(scope) {
 	var Requir_e = function(_scope) {
 		this._scope = _scope;
+		this._scope.requir_e = function(code) {
+			return code;
+		}
 		this.basedir = process.cwd();
 	}
 	Requir_e.prototype = {
 		eRoot: function(root) {
 			this.basedir = process.cwd() + "/" + root;
 		},
-		e: function(s) {
+		e:function(s) {
 			var path = s.replace(/\./g, "/");
 			var basedir = this.basedir;
-			console.log(basedir + "/" + path + ".js");
 			var code = fs.readFileSync(basedir + "/" + path + ".js", "utf8");
-			eval.call(this._scope, code);
+			this._scope.requir_e(eval(code));
 		}
 	};
-	requir = new Requir_e(scope);
+	requir = new Requir_e(arguments.callee);
 })(module.exports);
--- readme.md
+++ readme.md
@@ -4,7 +4,9 @@ Use this module to require javascript files and eval them in the current scope.
 All derived files then can use the same method to requir.e other subclasses.
 
 ``` js
-require("../index.js");
+require("requir.e");
+// set package root ./src
+requir.eRoot("src");
 
 // requires the file: ./my/domain/SomeClass.js
 requir.e("my.domain.SomeClass");
